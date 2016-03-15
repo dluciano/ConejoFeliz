fb1771f538c179a37031d875b534ec29e11b2dc6
Dawlin Peña Luciano <dawmusic19@gmail.com>
2016-03-15 13:37:48 -0400

    added gameover and music

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+var BK = 1;
+var PL = 2;
 /**
  * Returns a random number between min (inclusive) and max (exclusive)
  */
@@ -14,11 +16,12 @@ var Boomb = cc.Sprite.extend({
         });
         return true;
     },
-    update: function(dt){
-        this.runAction(cc.moveBy(0.5, cc.p(0, -1)));
+    update: function(dt) {
+        this.onUpdate();
+        this.runAction(cc.moveBy(0.5, cc.p(0, -10)));
         if(this.y <= 0)
             this.removeFromParent(true);
-    }
+    },
 });
 
 var HelloWorldLayer = cc.Layer.extend({
@@ -33,17 +36,18 @@ var HelloWorldLayer = cc.Layer.extend({
         //posicionando la imagen de fondo
         this.sprFondo = new cc.Sprite(res.fondo_png);
         this.sprFondo.setPosition(size.width / 2,size.height / 2);
-        this.addChild(this.sprFondo, 0);
+        this.addChild(this.sprFondo, 0, BK);
         
         //posicionando la imagen de fondo
         this.sprConejo = new cc.Sprite(res.conejo_png);
         this.sprConejo.setPosition(size.width / 2,size.height * 0.15);
-        this.addChild(this.sprConejo, 1);
+        this.addChild(this.sprConejo, 1, PL);
 
           //add a keyboard event listener to statusLabel
         cc.eventManager.addListener({
             event: cc.EventListener.KEYBOARD,
             onKeyPressed:  function(keyCode, event){
+                if(self.ended)return;
                 var mov = 8;
                 var s = .5;
                 if(keyCode === 39){
@@ -57,23 +61,50 @@ var HelloWorldLayer = cc.Layer.extend({
             }
         }, this); 
         
-        this.schedule(this.fallingObjs, 3);
+        this.schedule(this.fallingObjs, 1);
         
         return true;
     },
     fallingObjs: function() {
+        var self = this;
         var boomb = new Boomb(this.sprFondo.height);
         var min = this.sprFondo.x - this.sprFondo.width/2 + boomb.width/2;
         var max = this.sprFondo.x + this.sprFondo.width/2 - boomb.width/2;
+        boomb.onUpdate = function(){
+            if(cc.rectIntersectsRect(this.getBoundingBoxToWorld(), self.sprConejo.getBoundingBoxToWorld())){
+                 self.showGameOver();
+            }
+        };
         boomb.x = getRandomArbitrary(min, max);
+        if(this.ended)
+            return;
         this.addChild(boomb, 1);
-    }
+    },
+    showGameOver: function(){
+        cc.audioEngine.playEffect(res.gameover_mp3);
+        cc.audioEngine.stopMusic();
+        this.ended = true;
+        this.unscheduleAllCallbacks();
+        this.getChildren().forEach(function(c){
+            if(c.getTag() === BK || c.getTag() === PL) return;
+            c.unscheduleAllCallbacks();
+        });
+        var gm = new cc.Sprite(res.gameover_png);
+        gm.attr({
+            x: this.sprFondo.x,
+            y: this.sprFondo.y,
+            scale: 0.2
+        });
+        this.addChild(gm, 2);
+    },
+    ended: false,
 });
 
 var HelloWorldScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
         var layer = new HelloWorldLayer();
+        cc.audioEngine.playMusic(res.main_mp3, true);
         this.addChild(layer);
     }
 });
--- src/resource.js
+++ src/resource.js
@@ -2,6 +2,9 @@ var res = {
     bomba_png : "res/bomba.png",
     conejo_png : "res/conejo.png",
     fondo_png : "res/fondo.png",
+    gameover_png: "res/gameover.png",
+    gameover_mp3: "res/gameover.mp3",
+    main_mp3: "res/main.m4a"
 };
 
 var g_resources = [];
